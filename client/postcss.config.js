67b7ccd82e608e46e0d1fe1d7df2c87f9e5e2e84
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-08-15 23:37:57 +0800

    PostCSS MVP done
    1. My PostCSS setup is done following the lesson code I have a month ago
    2. Installed the necessary NPM packages and created postcss-sample.css containing sample code on how to use it
    3. Imported 3 fonts from Google Fonts
    4. Bare-minimum styling implemented to test and it works

---
+++ client/postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-preset-env')(
+      {
+        stage: 1
+      }
+    ),
+    require('precss'),
+    require('postcss-import'),
+    require('postcss-assets')(
+      {
+        loadPaths: ['dist/img']
+      }
+    ),
+    require('cssnano')(
+      {
+        preset: 'default'
+      }
+    )
+  ]
+};
